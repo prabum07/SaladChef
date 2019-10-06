a3f9431d9b113d4d12fcec1a3351868d3bcf7dd4
prabum07 <prabum07@users.noreply.github.com>
2019-10-06 12:45:29 +0530

    player1 Move

---
+++ Assets/Player.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody2D rb;
+    public int MoveAmount;
+    public List<char> VegetablesInHand = new List<char>();
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+     //   rb.AddForce(Vector2.up*MoveAmount, ForceMode2D.Force  );
+        rb.velocity = Vector3.zero;
+
+    }
+
+    // Update is called once per frame
+    public Vector3 dir;
+    void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            dir = Vector2.up;
+
+        }else
+        {
+            dir = Vector3.zero;
+        }
+         if(Input.GetKey(KeyCode.S))
+        {
+            dir = Vector2.down;
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            dir = Vector2.left;
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            dir = Vector2.right;
+        }
+        rb.velocity = dir*new Vector2(MoveAmount,MoveAmount);
+    }
+    void OnTriggerStay2D(Collider2D col)
+    {
+        char[] arr = col.gameObject.name.ToCharArray();
+        Debug.Log( arr[0]);
+        if (col.tag == "Veg")
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                if (VegetablesInHand.Count < 2)
+                {
+                    if (!VegetablesInHand.Contains(arr[0]))
+                    {
+                        VegetablesInHand.Add(arr[0]);
+                    }
+                }
+            }
+        }
+    }
+}
